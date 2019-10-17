8615f214d3df6eac0d20b63b278a78f6aabbd6d5
vikitorfg <victorgarzel@hotmail.com>
2019-10-17 15:36:15 -0300

    dele unecessary files, start boiler plate

---
+++ frontend/src/src/components/Body.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Body extends Component {
+  state = {};
+  render() {
+    return <div>Hey there I`m a body</div>;
+  }
+}
+
+export default Body;
---
+++ frontend/src/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  state = {};
+  render() {
+    return <div>Hey there I`m a Footer</div>;
+  }
+}
+
+export default Footer;

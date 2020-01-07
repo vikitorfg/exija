d3ab12bea07fe136dda810eadb38d153535909c0
vikitorfg <victorgarzel@hotmail.com>
2020-01-07 15:41:16 -0300

    feat: changed design to atomic

--- frontend/src/src/App.js
+++ frontend/src/src/App.js
@@ -1,20 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
-import Header from "./components/Header";
-import Body from "./components/Body";
-import Footer from "./components/Footer";
 
 class App extends Component {
   state = {};
 
   render() {
-    return (
-      <div>
-        <Header />
-        <Body />
-        <Footer />
-      </div>
-    );
+    return <div></div>;
   }
 }
 
